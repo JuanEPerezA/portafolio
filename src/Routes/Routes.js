fbf0cdf0d00c39aa98454ea2dc1cf45a588d391d
JuanEPerezA <juanepereza@gmail.com>
2024-03-19 15:20:45 -0500

    GHPage3

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 // import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import {BrowserRouter, Routes as Switch, Route} from 'react-router-dom';
+import {HashRouter as BrowserRouter, Routes as Switch, Route} from 'react-router-dom';
 import Home from './../Pages/Home';
 import About from './../Pages/About';
 import Contacto from './../Pages/Contacto';
