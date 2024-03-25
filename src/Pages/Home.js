9c12223d38bab00eebd61795c1ca70038b9aab42
JuanEPerezA <juanepereza@gmail.com>
2024-03-25 17:24:54 -0500

    Correcciones homeResponsive 1

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,11 +5,14 @@ import lenguaje from '../Config/lenguaje';
 const Home = ({idioma, setIdioma}) => {
   return (
     <>
-    {/* <Navbar idioma={idioma} setIdioma={setIdioma}/> */}
-    <div className="flex flex-col items-center h-[100vh] md:h-[90vh] lg:h-[80vh] xl:h-[71vh] justify-center aos-init aos-animate" data-aos="fade" style={{"paddingTop":"10vh"}}>
-        <img className="rounded-full w-[250px] h-[250px] 2xl:w-[280px] 2xl:h-[280px]" src="./Assets/Images/J.png" alt="Avatar"/>
-        <h3 className="mt-6 mb-1 text-5xl font-semibold darkMode">Juan Esteban Pérez Aguas</h3>
-        <h6 className="mb-4 text-2xl text-[#7B7B7B]">{lenguaje.cargo[`${idioma}`]}</h6>
+    <div className="flex flex-col items-center pt-[0vh] lg:pt-[10vh] h-[100vh] md:h-[90vh] lg:h-[80vh] xl:h-[71vh] justify-center aos-init aos-animate" data-aos="fade">
+        <img className="hidden sm:ml-6 sm:block rounded-full w-[250px] h-[250px] 2xl:w-[280px] 2xl:h-[280px]" src="./Assets/Images/J.png" alt="Avatar"/>
+        <img className="sm:hidden rounded-full imgAvatarSM" src="./Assets/Images/J.png" alt="Avatar"/>
+        <h3 className="hidden sm:ml-6 sm:block mt-6 mb-1 text-5xl font-semibold darkMode" style={{"textAlign":"center"}}>Juan Esteban Pérez Aguas</h3>
+        <h6 className="hidden sm:ml-6 sm:block mb-4 text-2xl text-[#7B7B7B]" style={{"textAlign":"center"}}>{lenguaje.cargo[`${idioma}`]}</h6>
+        <h3 className="sm:hidden mt-6 mb-1 text-4xl font-semibold darkMode" style={{"textAlign":"center"}}>Juan Esteban</h3>
+        <h3 className="sm:hidden text-4xl font-semibold darkMode" style={{"textAlign":"center"}}>Pérez Aguas</h3>
+        <h6 className="sm:hidden mb-4 text-2xl text-[#7B7B7B]" style={{"textAlign":"center"}}>{lenguaje.cargo[`${idioma}`]}</h6>
         <div className="flex space-x-3">
             <a href="https://api.whatsapp.com/send?phone=573117121720" target="_blank" rel="noopener noreferrer" className="alink">
                 <span className="socialbtn text-[#377c4d]">
